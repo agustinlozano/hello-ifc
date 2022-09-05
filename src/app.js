30c28c8bcdfa25c974f73e61d47af0d3e8f91a43
Agustin B. Lozano <agustinlozanoblua@gmail.com>
2022-09-05 15:45:36 -0300

    Add a new folder to put stuff there

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+import { IFCSLAB } from "web-ifc";
 import { Color } from "three";
 import { IfcViewerAPI } from "web-ifc-viewer";
 
@@ -10,16 +11,23 @@ viewer.axes.setAxes();
 viewer.grid.setGrid();
 viewer.IFC.setWasmPath("wasm/");
 
-const input = document.getElementById("file-input");
-
 window.ondblclick = () => viewer.IFC.selector.pickIfcItem(true);
 window.onmousemove = () => viewer.IFC.selector.prePickIfcItem();
 window.onclick = async () => {
   const {modelID, id} = await viewer.IFC.selector.pickIfcItem(true);
+
+  console.log(modelID, id);
+
   const props = await viewer.IFC.getProperties(modelID, id, true, false);
+
+  const {GlobalId, PredefinedType} = props;
   
   // Look at the properties
   console.log(props);
+  console.log(GlobalId)
+  console.log(PredefinedType)
+
+  getItemsOfAssembly(modelID, id);
 }
 
 viewer.clipper.active = true;
@@ -32,6 +40,8 @@ window.onkeydown = (event) => {
   }
 };
 
+const input = document.getElementById("file-input");
+
 // Loading user's models
 input.addEventListener(
   "change",
@@ -40,10 +50,32 @@ input.addEventListener(
     const file = changed.target.files[0];
     const ifcURL = URL.createObjectURL(file);
 
-    console.log(ifcURL)
-
-    viewer.IFC.loadIfcUrl(ifcURL);
+    const myModel = await viewer.IFC.loadIfcUrl(ifcURL);
+    
+    console.log('Lo que devuelve loadIfcUrl', myModel)
   },
 
   false
 );
+
+async function getItemsOfAssembly(modelID, groupID) {
+  const manager = viewer.IFC.loader.ifcManager
+  
+  // Get all ifcAssembly
+  //const asbs = await manager.getAllItemsOfType(modelID, IFCELEMENTASSEMBLY);
+  const asbs = await manager.getAllItemsOfType(modelID, IFCSLAB);
+
+  console.log('asbs', asbs)
+  
+  let asb, props;		
+  
+  for(asb of asbs) {
+    console.log(asb);
+    // props = await manager.getItemProperties(modelID, asb);
+    // console.log('console from Pablo code', props);
+  }
+
+  var guIDs = [];
+  
+  return guIDs;
+}
