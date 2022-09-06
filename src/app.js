a168318bf71acfa1018ae2fa4e24f79560c7d576
Agustin Lozano Blua <agustinlozanoblua@gmail.com>
2022-09-05 21:10:20 -0300

    Update app

--- src/app.js
+++ src/app.js
@@ -1,73 +1,35 @@
-import { IFCSLAB } from "web-ifc";
-import { Color } from "three";
-import { IfcViewerAPI } from "web-ifc-viewer";
+import { 
+  logAllSlabs,
+} from "./utils/getStuff";
+import viewer from "./utils/initViewer";
+import loadIfc from "./utils/loadIfc";
+import shortcuts from "./utils/shortcuts";
 
-const container = document.getElementById("viewer-container");
-const viewer = new IfcViewerAPI({
-  container,
-  backgroundColor: new Color(0xffffff),
-});
-viewer.axes.setAxes();
-viewer.grid.setGrid();
-viewer.IFC.setWasmPath("wasm/");
-
-window.ondblclick = () => viewer.IFC.selector.pickIfcItem(true);
+// Destaca los elementos del modelo que tengan hover
 window.onmousemove = () => viewer.IFC.selector.prePickIfcItem();
+
+// Deberia destacar el seleccionado y opacar el resto
+window.ondblclick = () => viewer.IFC.selector.highlightIfcItem(true);
+
+// Logea las propiedades
 window.onclick = async () => {
-  const {modelID, id} = await viewer.IFC.selector.pickIfcItem(true);
-
-  console.log(modelID, id);
-
-  const props = await viewer.IFC.getProperties(modelID, id, true, false);
-
-  const {GlobalId, PredefinedType} = props;
-  
-  // Look at the properties
-  console.log(props);
+  try {
+    const {modelID, id} = await viewer.IFC.selector.pickIfcItem(true);
+    const props = await viewer.IFC.getProperties(modelID, id, true, false);
+    
+    // Look at the properties
+    console.log(props);
+  } catch (error) {
+    null
+  }
 }
 
+// Por alguna razon lo incluyen
 viewer.clipper.active = true;
 
-window.onkeydown = (event) => {
-  if (event.code === "KeyP") {
-    viewer.clipper.createPlane();
-  } else if (event.code === "KeyO") {
-    viewer.clipper.deletePlane();
-  }
-};
-
-const input = document.getElementById("file-input");
+// My shortcuts
+window.onkeydown = shortcuts
 
 // Loading user's models
-input.addEventListener(
-  "change",
-
-  async (changed) => {
-    const file = changed.target.files[0];
-    const ifcURL = URL.createObjectURL(file);
-
-    const myModel = await viewer.IFC.loadIfcUrl(ifcURL);
-    
-    console.log('Lo que devuelve loadIfcUrl', myModel)
-  },
-
-  false
-);
-
-/**
- * Esta funcion encuentra todos los slabs de un modelo y
- * luego logea sus propiedades en la consola
- */
-async function logAllSlabs(modelID = 0) {
-  const manager = viewer.IFC.loader.ifcManager
-  
-  // Obtener todos los elementos del modelo segun el material
-  const slabsID = await manager.getAllItemsOfType(modelID, IFCSLAB);
-
-  console.log('My model slabs', slabsID)
-    
-  for(const slab of slabsID) {
-    const slabProps = await manager.getItemProperties(modelID, slab);
-    console.log(slabProps);
-  }
-}
+const input = document.getElementById("file-input");
+input.addEventListener("change", loadIfc, false);
