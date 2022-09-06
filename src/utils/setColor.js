1330a5240dca553b3c2b59355e5118445cfbc189
Agustin B. Lozano <agustinlozanoblua@gmail.com>
2022-09-06 16:16:46 -0300

    Update setColor function

--- src/utils/setColor.js
+++ src/utils/setColor.js
@@ -1,3 +1,13 @@
-const setColorOfGrup = (material, color = 0xcfcfcf) => {
+import viewer from "./initViewer"
 
+const setColorOfGrup = async (material, color = 0xcfcfcf) => {
+  const manager = viewer.IFC.loader.ifcManager
+  const IDs = await manager.getAllItemsOfType(0, material)
+
+  for (const ID of IDs) {
+    const props = await manager.getItemProperties(0, ID)
+    const { GlobalId } = props
+
+    //cambiar el color de los elementos
+  }
 }
