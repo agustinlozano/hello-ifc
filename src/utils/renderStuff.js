2927980bafe67b148bdbe964904910fe25910dca
Agustin B. Lozano <agustinlozanoblua@gmail.com>
2022-09-12 16:53:00 -0300

    Update module

--- src/utils/renderStuff.js
+++ src/utils/renderStuff.js
@@ -1,5 +1,6 @@
 const propsOutput = document.getElementById("props-output");
 const btzdOutput = document.getElementById("btzd-output");
+const propsSetOutput = document.getElementById("props-set-output");
 
 export const renderProps = (props) => {
   propsOutput.innerHTML = JSON.stringify(props, null, 2);
@@ -13,5 +14,16 @@ export const renderBtzd = (btzds) => {
     pre.innerHTML = JSON.stringify(btzd, null, 2);
     li.appendChild(pre);
     btzdOutput.appendChild(li);
+  }
+}
+
+export const renderBtzdV2 = (propsSet) => {
+  for (const btzd of propsSet) {
+    const li = document.createElement("li");
+    const pre = document.createElement("pre");
+
+    pre.innerHTML = JSON.stringify(btzd, null, 2);
+    li.appendChild(pre);
+    propsSetOutput.appendChild(li);
   }
 }
