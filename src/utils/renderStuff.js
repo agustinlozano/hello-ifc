1ea7645b214a513a7cd1e312870c9f24b8c80166
Agustin B. Lozano <agustinlozanoblua@gmail.com>
2022-09-13 11:59:10 -0300

    Update functions according to the HTML changes

--- src/utils/renderStuff.js
+++ src/utils/renderStuff.js
@@ -1,29 +1,43 @@
-const propsOutput = document.getElementById('props-output')
-const btzdOutput = document.getElementById('btzd-output')
-const propsSetOutput = document.getElementById('props-set-output')
+const $propsOutput = document.getElementById('props-output')
+
+const $btzBlocksOutput = document.getElementById('btz-blocks-output')
+const $btzBlocksLength = document.getElementById('btz-blocks-length')
+
+const $propSetOutput = document.getElementById('prop-set-output')
+const $propSetLength = document.getElementById('prop-set-length')
 
 export function renderProps (props) {
-  propsOutput.innerHTML = JSON.stringify(props, null, 2)
+  $propsOutput.innerHTML = JSON.stringify(props, null, 2)
 }
 
 export function renderBtzd (btzds) {
+
+  if (btzds.length === 0) return null
+
+  $btzBlocksLength.innerHTML = btzds.length
+
   for (const btzd of btzds) {
     const li = document.createElement('li')
     const pre = document.createElement('pre')
 
     pre.innerHTML = JSON.stringify(btzd, null, 2)
     li.appendChild(pre)
-    btzdOutput.appendChild(li)
+    $btzBlocksOutput.appendChild(li)
   }
 }
 
 export function renderBtzdV2 (propsSet) {
+
+  if (propsSet.length === 0) return null
+
+  $propSetLength.innerHTML = propsSet.length
+
   for (const btzd of propsSet) {
     const li = document.createElement('li')
     const pre = document.createElement('pre')
 
     pre.innerHTML = JSON.stringify(btzd, null, 2)
     li.appendChild(pre)
-    propsSetOutput.appendChild(li)
+    $propSetOutput.appendChild(li)
   }
 }
