ad7fe2db9d5166f1089cd2bbbb463b67dbdb2595
Agustin B. Lozano <agustinlozanoblua@gmail.com>
2022-09-19 16:13:25 -0300

    Update loadIfc from config

--- src/config/loadIfc.js
+++ src/config/loadIfc.js
@@ -7,21 +7,11 @@ import {
   sortProperties
 } from '../modules/sortStuff'
 import viewer from './initViewer'
-import concatAll from '../utils/concatAll'
-import { btzHash } from '../modules/hashStuff'
 
 async function loadIfc (changed) {
   const file = changed.target.files[0]
   const ifcURL = URL.createObjectURL(file)
   const myModel = await viewer.IFC.loadIfcUrl(ifcURL)
-
-  const guids = ['1rj4YnIav09AtqlDAEtLZa', '1rj4YnIav09AtqlDAEtLco']
-  const aBtzDesc = 'Losa planta baja amarilla'
-
-  const concatened = concatAll(guids, aBtzDesc)
-
-  console.log(await btzHash(concatened))
-  console.log(concatened.length)
 
   // Clasificacion de informacion cruda del modelo IFC
   const rawBtzDescription = await getPropSingleValue('description')
@@ -37,7 +27,7 @@ async function loadIfc (changed) {
     myModel.modelID)
 
   const sortedBlocks = sortProperties(filterProps, rawBtzDescription)
-  const btzBlocks = buildBtzBlocks(rawPropsSet, sortedBlocks)
+  const btzBlocks = await buildBtzBlocks(rawPropsSet, sortedBlocks)
 
   renderJsonData(btzBlocks, 'btzBlock')
   renderJsonData(rawPropsSet, 'propSet')
