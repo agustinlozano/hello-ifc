b5f2fecbb57a0e529f1c54bf5480f30e0d34dbbb
Agustin B. Lozano <agustinlozanoblua@gmail.com>
2022-09-22 16:28:50 -0300

    Add a new function to increment base35 codes

--- src/modules/blockCoding.js
+++ src/modules/blockCoding.js
@@ -1,4 +1,5 @@
-function generateCombinations (length) {
+// A function that generates all the combinations of base 36 numbers with a length of 5 characters
+export function generateCombinations (length) {
   const combinations = []
   const alphabet = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
   const base = alphabet.length
@@ -19,4 +20,39 @@ function generateCombinations (length) {
   return combinations
 }
 
-export default generateCombinations
+// A function that returns one combination of base 36 numbers with a length of 5 characters
+export function generateCombination () {
+  const alphabet = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  const base = alphabet.length
+  let combination = ''
+
+  for (let i = 0; i < 5; i++) {
+    combination += alphabet[Math.floor(Math.random() * base)]
+  }
+
+  return combination
+}
+
+// A function that increments a combination of base 36 numbers with a length of 5 characters
+export function incrementCombination (combinationFromUser) {
+  if (combinationFromUser.length !== 5) {
+    console.error('The combination must have a length of 5 characters')
+  }
+
+  const combination = combinationFromUser.toUpperCase()
+  const alphabet = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  const combinationArray = combination.split('')
+  let i = 4
+
+  while (i >= 0) {
+    if (combinationArray[i] === 'Z') {
+      combinationArray[i] = '0'
+      i--
+    } else {
+      combinationArray[i] = alphabet[alphabet.indexOf(combinationArray[i]) + 1]
+      break
+    }
+  }
+
+  return combinationArray.join('')
+}
