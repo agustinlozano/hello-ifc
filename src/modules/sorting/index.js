1991cb8df263ddf9d877e2c40d65c98a327db161
Agustin Lozano Blua <agustinlozanoblua@gmail.com>
2022-09-23 07:06:53 -0300

    Add a new file to export module functions

---
+++ src/modules/sorting/index.js
@@ -0,0 +1,19 @@
+// index file for exporting all sorting functions
+export { bimtrazerSort, bimtrazerSortDev } from './bimtrazerSort'
+export {
+  sortPropertiesV4,
+  buildBtzBlocksV3,
+  buildBtzBlocksV4,
+  sortProperties,
+  filterProps,
+  filterDictionary,
+  filterPropertiesIds
+} from './sortStuff'
+
+export {
+  handleFullSortPropertyCase,
+  handleSortPropertyCaseV2,
+  handleFilterDictionaryCase,
+  handleFullSortDictionaryCase,
+  linkProperties
+} from './utils'
