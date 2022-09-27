64c547b45337705fa7f472673564eb7702db0b21
Agustin Lozano Blua <agustinlozanoblua@gmail.com>
2022-09-27 20:39:14 -0300

    Fix an error by modifying body from fetch option

--- src/services/storeBlocks.js
+++ src/services/storeBlocks.js
@@ -2,27 +2,17 @@ export async function storeBlocks (projectId, data, serviceType) {
   const URL = 'http://projects.bimtrazer.com/api/PostDataProj'
   const options = {
     method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: {
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
       PROJ: projectId,
       ID: serviceType,
-      DATA: JSON.stringify(data)
-    }
+      DATA: data
+    })
   }
 
   try {
     const res = await fetch(URL, options)
-    const jsonResponse = await res.json()
-
-    console.log(jsonResponse)
-
-    if (jsonResponse.DESCRIPCION === 'Successful' && jsonResponse.ID === '00') {
-      return jsonResponse
-    }
-
-    return null
+    return await res.json()
   } catch (error) {
     console.error('There was an error: ', error)
     return null
